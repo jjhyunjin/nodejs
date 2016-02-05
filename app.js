48ae6c42419f13162844d0d5f0c8a4ea224f134b
jjyunjin <jjhyunjin@gmail.com>
2016-02-05 17:40:39 +0900

    mongocb setting

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_DB);
+var db = mongoose.connection;
+db.once("open", function() {
+  console.log("DB connected!");
+});
+db.on("error", function (err){
+  console.log("DB Error : ", err);
+});
+
+var dataSchema = mongoose.Schema({
+  name : String,
+  count : Number
+});
+
+var Data = mongoose.model('data', dataSchema);
+Data.findOne({name :"myData"}, function(err, data){
+  if(err) return console.log("Data Error : " , err);
+  if(!data){
+    Data.create({name :"myData", count:0}, function (err, data){
+      if(err) return console.log("Data Error : ", err);
+      console.log("Counter initialized : ", data);
+    });
+  }
+});
 
 app.set("view engine", 'ejs');
 app.use(express.static(__dirname + '/public'));
