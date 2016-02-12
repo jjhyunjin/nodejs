94b1bc8c81dd62482ef3077bd7eba02235a9ba95
jjyunjin <jjhyunjin@gmail.com>
2016-02-12 17:53:49 +0900

    login

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 // import modules
-var express = require('express');
-var path = require('path');
-var app = express();
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
+var express        = require('express');
+var path           = require('path');
+var app            = express();
+var mongoose       = require('mongoose');
+var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
+var passport       = require('passport');
+var session        = require('express-session');
+var flash          = require('connect-flash');
+var async          = require('async');
 
 // connect database
 mongoose.connect(process.env.MONGO_DB);
@@ -24,6 +28,45 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+app.use(flash());
+
+app.use(session({secret:'MySecret'}));
+app.use(passport.initialize());
+app.use(passport.sesstion());
+
+passport.serializeUser(function(user, done){
+  done(null, user.id);
+});
+passport.deserializeUser(function(id, done){
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+var LocalStragegy = require('passport-local').Strategy;
+passport.use('local-login',
+  new LocalStrategy({
+    usernameField : 'email',
+    passwordField : 'password',
+    passReqToCallback : true
+  },
+  function(req, email, password, done){
+      User.findOne({"email" : email}, function(err, user){
+        if(err) return done(err);
+
+        if(!user){
+          req.flash("email", req.body.email);
+          return done(null, false, req.flash('loginError', 'No user found.'));
+        }
+        if(user.password != password){
+          req.falsh("email", req.body.email);
+          return done(null, false, req.flash('loginError', 'Password does not Match.'));
+        }
+        return done(null, user);
+      });
+    }
+  )
+);
 
 // model setting
 var postSchema = mongoose.Schema({
@@ -34,10 +77,38 @@ var postSchema = mongoose.Schema({
 });
 var Post = mongoose.model('post', postSchema);
 
+var userSchema = mongoose.Schema({
+  email: {type:String, required:true, unique:true},
+  nickname: {type:String, required:true, unique:true},
+  password: {type:String, required:true},
+  createdAt: {type:Date, default:Date.now}
+});
+var User = mongoose.model('user', userSchema);
+
 // lists
 app.get('/', function(req,res) {
   res.redirect('/posts');
 });
+app.get('/login', function(req, res){
+  res.render('login/login', {email:req.flash("email")[0], loginError:req.flash('loginError')});
+});
+app.post('/login', function(req, res, next){
+  req.flash("email");
+  if(req.body.email.length === 0 ||
+      req.body.password.length === 0){
+        req.flash("email", req.body.email);
+        req.flash("loginError", "Please enter both email and password.");
+        res.redirect('/login');
+  }else{
+    next();
+  }
+}, passport.authenticate('local-login', {
+  successRedirect : '/posts',
+  failureRedirect : '/login',
+  failureFlash : true
+ })
+);
+
 app.get('/posts', function(req, res){
   Post.find({}).sort('-createdAt').exec(function (err, posts) {
       if(err) return res.json({success:false, message:err});
